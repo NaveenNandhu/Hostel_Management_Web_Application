fc12c4c8d3d31046ad19579b068939f36f9e6561
NaveenNandhu <nandhanaveengasc41@gmail.com>
2024-06-19 00:45:44 +0530

    Update export_monthly.php

--- hospro/export_monthly.php
+++ hospro/export_monthly.php
@@ -4,51 +4,52 @@ $username = "root";
 $password = "";
 $dbname = "products";
 $connec = new mysqli($servername, $username, $password, $dbname);
+
+if ($connec->connect_error) {
+    die("Connection failed: " . $connec->connect_error);
+}
+
 extract($_GET);
+
 $sql = "SELECT * FROM `hostel_2024`";
 $result = $connec->query($sql);
+
 $row = [];
 if ($result->num_rows > 0) {
     $row = $result->fetch_all(MYSQLI_ASSOC);
 }
-echo "<table class='table table-hover table-bordered'>";
+
+// Set headers for the download
+$file_name = $date . "_Totalstock.xls";
+header("Content-Disposition: attachment; filename=\"$file_name\"");
+header("Content-Type: application/vnd.ms-excel");
+
+// Start output buffering
+ob_start();
+
+echo "<table>";
 echo "<thead>";
 echo "<tr>";
-echo "<th scope='col'>";
-echo "<center>Product Id</center>";
-echo "</th>";
-echo "<th scope='col'>";
-echo "<center>Product Name</center>";
-echo "</th>";
-echo "<th scope='col'>";
-echo "<center>Quantity</center>";
-echo "</th>";
+echo "<th>Product Id</th>";
+echo "<th>Product Name</th>";
+echo "<th>Quantity</th>";
 echo "</tr>";
 echo "</thead>";
 echo "<tbody>";
-if (!empty($row))
+
+if (!empty($row)) {
     foreach ($row as $rows) {
         echo "<tr>";
-        echo "<td>";
-        echo "<center>";
-        echo $rows['pid'];
-        echo "</center>";
-        echo "</td>";
-        echo "<td>";
-        echo "<center>";
-        echo $rows['pname'];
-        echo "</center>";
-        echo "</td>";
-        echo "<td>";
-        echo "<center>";
-        echo $rows[$date];
-        echo $rows['pkg'];
-        echo "</center>";
-        echo "</td>";
+        echo "<td>" . $rows['pid'] . "</td>";
+        echo "<td>" . $rows['pname'] . "</td>";
+        echo "<td>" . $rows[$date] . $rows['pkg'] . "</td>";
         echo "</tr>";
     }
-echo "<tbody>";
-$file_name = "$date _ Totalstock.xls";
-header("Content-Disposition: attachment; filename=\"$file_name\"");
-header("Content-Type: application/vnd.ms-excel");
+}
+
+echo "</tbody>";
+echo "</table>";
+
+// Flush the output buffer
+ob_end_flush();
 ?>
